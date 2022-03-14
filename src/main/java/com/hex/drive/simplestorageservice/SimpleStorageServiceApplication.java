48f670694c36f2111ccf59b07433f8d93da0d9f5
hector-silvar <hector.silvar@mediamonks.com>
2022-03-14 11:08:19 -0600

    Adding basic springboot template

---
+++ src/main/java/com/hex/drive/simplestorageservice/SimpleStorageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hex.drive.simplestorageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleStorageServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SimpleStorageServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/hex/drive/simplestorageservice/SimpleStorageServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.hex.drive.simplestorageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleStorageServiceApplicationTests {
+
+  @Test void contextLoads() {
+  }
+
+}
